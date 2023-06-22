76c15082c4e49cd223d3c09877e5caf6c71a8ae0
Vinicius de Souza <viniciusdesouzafernando@gmail.com>
2023-06-22 00:33:04 -0300

    Realizada conexão com MongoDB, modelagem de schema, implementação de CRUD.

--- server.js
+++ server.js
@@ -1,12 +1,90 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/productModel");
 const app = express();
 
-//router routes
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello Node API");
+//ROUTES
+
+//Fetch all products
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+//Fetch single product
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
+//Add Product
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Update Product
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Cannot find any item with ${id}` });
+    }
+    const updateProduct = await Product.findById(id);
+
+    res.status(200).json(updateProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Delete Product
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Cannot find any item with ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//MongoDB Connection
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@gamepointapi.bh2eoin.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Listening on port 3000");
+    });
+    console.log("Succesfully connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(error);
+  });
