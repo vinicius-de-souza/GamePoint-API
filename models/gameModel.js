be078e1ae21f940d7e127c7cce86a07ab03c7ffa
Vinicius de Souza <viniciusdesouzafernando@gmail.com>
2023-06-23 04:04:04 -0300

    Fix gameModel

--- models/gameModel.js
+++ models/gameModel.js
@@ -1,24 +1,22 @@
 const mongoose = require("mongoose");
 
 const gameSchema = mongoose.Schema({
-  game: {
-    name: {
-      type: "string",
-      required: [true, "Por favor, insira um nome"],
-    },
-    rating: {
-      type: Number,
-      required: [true, "Por favor, insira uma nota"],
-      default: 0,
-    },
-    desc: {
-      type: String,
-      required: [true, "Por favor, insira uma descrição"],
-    },
-    image: {
-      type: String,
-      required: [true, "Por favor, insira uma imagem"],
-    },
+  name: {
+    type: "string",
+    required: [true, "Por favor, insira um nome"],
+  },
+  rating: {
+    type: Number,
+    required: [true, "Por favor, insira uma nota"],
+    default: 0,
+  },
+  desc: {
+    type: String,
+    required: [true, "Por favor, insira uma descrição"],
+  },
+  image: {
+    type: String,
+    required: [true, "Por favor, insira uma imagem"],
   },
 });
 
